06c76c4b8d4a03879c3fbb8b5743bf3d77a4eb4e
Hugn71205 <diablo.hugn@gmail.com>
2025-05-23 10:14:53 +0700

    first commit

---
+++ src/components/commonAdmin/Header.tsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+
+const AdminHeader = () => {
+  return (
+    <header className="bg-white w-full shadow-md flex p-4 relative z-50">
+      <div className="logo w-1/5">Admin</div>
+      <div className="right-header w-4/5 flex justify-between">
+        <form>
+          <input
+            className="border rounded-md w-[350px] px-2 py-1"
+            type="text"
+            placeholder="Tìm kiếm"
+          />
+        </form>
+        <div className="space-x-4">
+          <Link to={'login'}>Login</Link>
+          <Link to={'register'}>Register</Link>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default AdminHeader;
---
+++ src/routes/main.route.tsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router-dom"
+import AdminLayout from "../components/layouts/AdminLayout"
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AdminLayout />,
+    children: [
+      
+    ],
+  }
+])
